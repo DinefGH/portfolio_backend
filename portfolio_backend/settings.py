9c3fea8889b896d9ddaf6c61a3d882e9e795cfb4
DinefGH <112215706+DinefGH@users.noreply.github.com>
2024-11-14 23:44:53 +0100

    static root

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -128,7 +128,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-STATIC_URL = '/static/'
 
 
 # Internationalization
@@ -147,6 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
